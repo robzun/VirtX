e81fb4c16ea10d4074e67528b9ee8a0c072a2ebe
Roberto <robzun03@gmail.com>
2025-05-01 13:40:12 -0600

    YA POR FIN QUEDARON LOS TESTS AAAAAAAAA

--- Assets/Tests/ButtonsBehavTest.cs
+++ Assets/Tests/ButtonsBehavTest.cs
@@ -1,46 +1,100 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 public class ButtonsBehavTests
 {
     private class MockSceneLoader : ISceneLoader
     {
-        public int lastSceneIndex = -1;
-        public bool quitCalled = false;
+        public int? LastSceneIndex { get; private set; }
+        public bool QuitCalled { get; private set; } = false;
 
         public void LoadScene(int buildIndex)
         {
-            lastSceneIndex = buildIndex;
+            LastSceneIndex = buildIndex;
         }
 
         public void Quit()
         {
-            quitCalled = true;
+            QuitCalled = true;
         }
     }
 
-    [Test]
-    public void GoToMainMenu_CallsScene0()
+    private MockSceneLoader mockLoader;
+
+    [SetUp]
+    public void Setup()
     {
-        var mockLoader = new MockSceneLoader();
+        mockLoader = new MockSceneLoader();
         ButtonsBehav.sceneLoader = mockLoader;
+    }
 
+    [Test]
+    public void GoToMainMenu_LoadsScene0()
+    {
         ButtonsBehav.GoToMainMenu();
+        Assert.AreEqual(0, mockLoader.LastSceneIndex);
+    }
 
-        Assert.AreEqual(0, mockLoader.lastSceneIndex);
+    [Test]
+    public void GoToUV01_LoadsScene1()
+    {
+        ButtonsBehav.GoToUV01();
+        Assert.AreEqual(1, mockLoader.LastSceneIndex);
+    }
+
+    [Test]
+    public void GoToCatMenu_LoadsScene2()
+    {
+        ButtonsBehav.GoToCatMenu();
+        Assert.AreEqual(2, mockLoader.LastSceneIndex);
+    }
+
+    [Test]
+    public void GoToOrgMenu_LoadsScene3()
+    {
+        ButtonsBehav.GoToOrgMenu();
+        Assert.AreEqual(3, mockLoader.LastSceneIndex);
+    }
+
+    [Test]
+    public void GoToLogin_LoadsScene4()
+    {
+        ButtonsBehav.GoToLogin();
+        Assert.AreEqual(4, mockLoader.LastSceneIndex);
+    }
+
+    [Test]
+    public void GoToUV02_LoadsScene5()
+    {
+        ButtonsBehav.GoToUV02();
+        Assert.AreEqual(5, mockLoader.LastSceneIndex);
+    }
+
+    [Test]
+    public void GoToUV03_LoadsScene6()
+    {
+        ButtonsBehav.GoToUV03();
+        Assert.AreEqual(6, mockLoader.LastSceneIndex);
+    }
+
+    [Test]
+    public void GoToCultMenu_LoadsScene7()
+    {
+        ButtonsBehav.GoToCultMenu();
+        Assert.AreEqual(7, mockLoader.LastSceneIndex);
+    }
+
+    [Test]
+    public void GoToSJU01_LoadsScene8()
+    {
+        ButtonsBehav.GoToSJU01();
+        Assert.AreEqual(8, mockLoader.LastSceneIndex);
     }
 
     [Test]
     public void ExitProgram_CallsQuit()
     {
-        var mockLoader = new MockSceneLoader();
-        ButtonsBehav.sceneLoader = mockLoader;
-
         ButtonsBehav.ExitProgram();
-
-        Assert.IsTrue(mockLoader.quitCalled);
+        Assert.IsTrue(mockLoader.QuitCalled);
     }
 }
---
+++ Assets/Tests/VideoHandlerTest.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.TestTools;
+
+public class VideoHandlerTests
+{
+    private GameObject videoObj;
+    private VideoHandler handler;
+    private VideoPlayer player;
+    private Button button;
+    private Sprite startSprite;
+    private Sprite stopSprite;
+    private GameObject previewHud;
+    private GameObject UIElements;
+    private GameObject interactionHud;
+    private GameObject pauseButton;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Crear objeto de prueba con componentes
+        videoObj = new GameObject("VideoObject");
+        player = videoObj.AddComponent<VideoPlayer>();
+        handler = videoObj.AddComponent<VideoHandler>();
+
+        // Crear botón con imagen
+        var buttonGO = new GameObject("Button");
+        button = buttonGO.AddComponent<Button>();
+        button.image = buttonGO.AddComponent<Image>();
+
+        // Sprites ficticios para pruebas
+        Texture2D testTexture = new Texture2D(10, 10);
+        handler.startSprite = Sprite.Create(testTexture, new Rect(0, 0, 10, 10), Vector2.zero);
+        handler.stopSprite = Sprite.Create(testTexture, new Rect(0, 0, 10, 10), Vector2.zero);
+
+        // Mock UI GameObjects
+        previewHud = new GameObject("PreviewHUD");
+        UIElements = new GameObject("UIElements");
+        interactionHud = new GameObject("InteractionHUD");
+        pauseButton = new GameObject("PauseButton");
+
+        // Asignar referencias
+        handler.button = button;
+        handler.startSprite = startSprite;
+        handler.stopSprite = stopSprite;
+        handler.previewHud = previewHud;
+        handler.UIElements = UIElements;
+        handler.interactionHud = interactionHud;
+        handler.pauseButton = pauseButton;
+
+        // Simular estado inicial
+        previewHud.SetActive(true);
+        UIElements.SetActive(false);
+        interactionHud.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.DestroyImmediate(videoObj);
+        Object.DestroyImmediate(button.gameObject);
+        Object.DestroyImmediate(previewHud);
+        Object.DestroyImmediate(UIElements);
+        Object.DestroyImmediate(interactionHud);
+        Object.DestroyImmediate(pauseButton);
+    }
+
+    [UnityTest]
+    public System.Collections.IEnumerator StartVideo_HidesPreviewAndShowsUI()
+    {
+        handler.StartVideo();
+        yield return null;
+
+        Assert.IsFalse(previewHud.activeSelf);
+        Assert.IsTrue(UIElements.activeSelf);
+    }
+
+    [UnityTest]
+    public System.Collections.IEnumerator PausePlayVideo_TogglesPlayPauseState()
+    {
+        Assert.AreEqual(stopSprite, button.image.sprite);
+
+        handler.PausePlayVideo();
+        yield return null;
+        
+        Assert.AreEqual(startSprite, button.image.sprite);
+    }
+
+    [UnityTest]
+    public System.Collections.IEnumerator VideoEnded_EnablesInteractionUI()
+    {
+        handler.SendMessage("VideoEnded", player);
+        yield return null;
+
+        Assert.IsTrue(interactionHud.activeSelf);
+        Assert.IsFalse(pauseButton.activeSelf);
+    }
+}
